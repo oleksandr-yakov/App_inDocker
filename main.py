cd4507f88f1c0d0d57606ef7dd6b4428164e6d9f
oleksandr-yakov <alex.yakov1109@gmail.com>
2022-10-12 21:26:06 +0300

    update main.py and ingex.html

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-server = Flask(__name__)
+server = Flask(__name__, template_folder="www")
 
 
 @server.route("/")
 def start_server():
-    return "You are calling me from" + request.remote_addr + "\n"
+    return render_template("index.html")
+    # return "You are calling me from" + request.remote_addr + "\n"
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=3234)
+    server.run(host="0.0.0.0", port=3294)
